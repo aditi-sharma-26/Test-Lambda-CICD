ca079cfe36bdb71c6c4fc530a3332931e8029fca
Aditi Sharma <aditi.sharma@lilly.com>
2025-02-06 14:13:29 +0530

    Final commit

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -6,5 +6,5 @@ def lambda_handler(event, context):
     print("Hello this is lambda execution.")
     return {
       'statusCode': 200,
-      'body': json.dumps('Hello from Lambda!')
+      'body': json.dumps('Hello from Lambda! From Github Actions')
     }
